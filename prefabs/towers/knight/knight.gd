f502a137b51a77d7542dd62a376c1c9573cf770e
thiagola92 <thiagola92@gmail.com>
2023-07-28 02:03:00 -0300

    Fix knight movements

--- prefabs/towers/knight/knight.gd
+++ prefabs/towers/knight/knight.gd
@@ -7,17 +7,25 @@ func _init() -> void:
 
 func get_attack_tiles(tile: Vector2i) -> Array[Vector2i]:
 	return [
-		tile + Vector2i(-2, 1),
 		tile + Vector2i(2, 1),
+		tile + Vector2i(2, -1),
+		tile + Vector2i(-2, 1),
 		tile + Vector2i(-2, -1),
-		tile + Vector2i(-2, -1)
+		tile + Vector2i(1, 2),
+		tile + Vector2i(1, -2),
+		tile + Vector2i(-1, 2),
+		tile + Vector2i(-1, -2)
 	]
 
 
 func get_move_tiles(tile: Vector2i) -> Array[Vector2i]:
 	return [
-		tile + Vector2i(-2, 1),
 		tile + Vector2i(2, 1),
+		tile + Vector2i(2, -1),
+		tile + Vector2i(-2, 1),
 		tile + Vector2i(-2, -1),
-		tile + Vector2i(-2, -1)
+		tile + Vector2i(1, 2),
+		tile + Vector2i(1, -2),
+		tile + Vector2i(-1, 2),
+		tile + Vector2i(-1, -2)
 	]
